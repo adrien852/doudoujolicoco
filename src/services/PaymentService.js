c22a636c256ec6d4a7698f5d93db51b40a1b7e84
Adrien <adrien@traveldepartment.ie>
2023-10-06 17:22:38 +0100

    HMAC security added to checkout API

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -1,5 +1,12 @@
 const API = import.meta.env.VITE_EXPRESS_API_URL;
+const publicKey = import.meta.env.VITE_API_PUBLIC_KEY;
+const privateKey = import.meta.env.VITE_API_PRIVATE_KEY;
 import axios from 'axios'
+import CryptoJS from 'crypto-js';
+
+function generateHmac(privateKey, payload){
+    return CryptoJS.enc.Hex.stringify(CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, privateKey).update(payload).finalize());
+}
 
 export async function getBraintreeToken() {
 
@@ -8,8 +15,8 @@ export async function getBraintreeToken() {
 }
 
 export async function checkout(nonce, items) {
-
-    items = items.map((item) => item = {'normalized': item.normalized, 'qty': 1})
-    const response = await axios.post(API+'/payment/checkout', {paymentMethodNonce: nonce, cartItems: items});
+    items = items.map((item) => item = {'normalized': item.normalized, 'qty': 1});
+    const payload = {paymentMethodNonce: nonce, cartItems: items};
+    const response = await axios.post(API+'/payment/checkout', {hmac: generateHmac(privateKey, payload), publicKey: publicKey, payload: payload});
     return response.data;
 }
