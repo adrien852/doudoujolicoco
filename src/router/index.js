7727adae707cc878b594d074adc6366f89916c78
Adrien <adrien@traveldepartment.ie>
2023-10-17 15:51:54 +0100

    Router mode for prod

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import CategoriesView from '@/views/CategoriesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'history',
   routes: [
     {
       path: '/',
